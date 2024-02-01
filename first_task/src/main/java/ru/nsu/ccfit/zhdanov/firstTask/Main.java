4198466d7429313acf9950599dcb2c01294a5829
Denis <denis.04@yandex.ru>
2024-02-01 21:14:21 +0700

    change loop on streamAPI methods

--- first_task/src/main/java/ru/nsu/ccfit/zhdanov/firstTask/Main.java
+++ first_task/src/main/java/ru/nsu/ccfit/zhdanov/firstTask/Main.java
@@ -31,10 +31,14 @@ public class Main {
 
     try (FileWriter csvWriter = new FileWriter("out.csv")) {
       int totalWordsQuantity = tokensFreq.values().stream().mapToInt(Integer::intValue).sum();
-      for (val entry : tokensFreq.entrySet().stream().sorted(Map.Entry.comparingByValue()).toArray(Map.Entry[]::new)) {
-        int freq = (int) entry.getValue();
-        csvWriter.write(String.format("%s,%d,%s%n", entry.getKey(), freq, String.valueOf((float) freq / totalWordsQuantity * 100).replace(',', '.')));
-      }
+      tokensFreq.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEachOrdered((entry) -> {
+        try {
+          csvWriter.write(String.format("%s,%d,%s%n", entry.getKey(), entry.getValue(), String.
+                  valueOf((float) entry.getValue() / totalWordsQuantity * 100).replace(',', '.')));
+        } catch (IOException e) {
+          System.out.println(e.getMessage());
+        }
+      });
     } catch (IOException e) {
       System.out.println(e.getMessage());
     }
