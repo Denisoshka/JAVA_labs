170542408fc6c5da02042896432bb1c0d1ee8ce9
Denis <denis.04@yandex.ru>
2024-02-02 15:51:49 +0700

    refactor lab

--- first_task/src/main/java/ru/nsu/ccfit/zhdanov/firstTask/Main.java
+++ first_task/src/main/java/ru/nsu/ccfit/zhdanov/firstTask/Main.java
@@ -1,6 +1,5 @@
 package ru.nsu.ccfit.zhdanov.firstTask;
 
-import lombok.val;
 import ru.nsu.ccfit.zhdanov.firstTask.exceptions.IncorrectProgramArgs;
 
 import java.io.*;
@@ -13,35 +12,41 @@ public class Main {
     if (args.length != 1 || args[0].isEmpty()) {
       throw new IncorrectProgramArgs();
     }
+
     Map<String, Integer> tokensFreq = new HashMap<>();
-    try (val scanner = new BufferedReader(new FileInputStream(args[0]))) {
-      while (scanner.hasNextLine()) {
-        val line = scanner.nextLine().split("\\P{Alnum}+");
-        for (val token : line) {
+    try (BufferedReader input = new BufferedReader(new FileReader(args[0]))) {
+      String line;
+      while ((line = input.readLine()) != null) {
+        for (String token : line.split("\\P{Alnum}+")) {
           if (!token.isEmpty()) {
-            val lowToken = token.toLowerCase();
-            tokensFreq.put(lowToken, tokensFreq.getOrDefault(lowToken, 0) + 1);
+            token = token.toLowerCase();
+            tokensFreq.put(token, tokensFreq.getOrDefault(token, 0) + 1);
           }
         }
       }
     } catch (IOException e) {
-      System.out.println(e.getMessage());
+      e.printStackTrace(System.err);
       return;
     }
 
-    try (BufferedWriter csvWriter = new BufferdFileWriter("out.csv")) {
+    try (BufferedWriter csvWriter = new BufferedWriter(new FileWriter("out.csv"))) {
       int totalWordsQuantity = tokensFreq.values().stream().mapToInt(Integer::intValue).sum();
-      tokensFreq.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEachOrdered((entry) -> {
+      String key;
+      int value;
+      for (Map.Entry<String, Integer> pair : tokensFreq.entrySet().stream().sorted(Map.Entry.comparingByValue()).toArray(Map.Entry[]::new)) {
+        key = pair.getKey();
+        value = pair.getValue();
         try {
-          csvWriter.write(String.format("%s,%d,%s%n", entry.getKey(), entry.getValue(), String.
-                  valueOf((float) entry.getValue() / totalWordsQuantity * 100).replace(',', '.')));
+          csvWriter.write(String.format("%s,%d,%s%n",key , value, String.
+                  valueOf((float) value / totalWordsQuantity * 100).replace(',', '.')));
         } catch (IOException e) {
-          e.printStackTrace();
-          System.out.println();
+          e.printStackTrace(System.err);
+          return;
         }
-      });
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
+      }
+      csvWriter.flush();
+    }catch ( IOException e ){
+      e.printStackTrace(System.err);
     }
   }
 }
