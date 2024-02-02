4362e1d470babca36d0f92ce9b0c8dc56dc41ad1
Denis <denis.04@yandex.ru>
2024-02-02 15:04:53 +0700

    add .idea in gitignore

--- first_task/src/main/java/ru/nsu/ccfit/zhdanov/firstTask/Main.java
+++ first_task/src/main/java/ru/nsu/ccfit/zhdanov/firstTask/Main.java
@@ -6,7 +6,7 @@ import ru.nsu.ccfit.zhdanov.firstTask.exceptions.IncorrectProgramArgs;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
+
 
 public class Main {
   public static void main(String[] args) {
@@ -14,7 +14,7 @@ public class Main {
       throw new IncorrectProgramArgs();
     }
     Map<String, Integer> tokensFreq = new HashMap<>();
-    try (val scanner = new Scanner(new FileInputStream(args[0]))) {
+    try (val scanner = new BufferedReader(new FileInputStream(args[0]))) {
       while (scanner.hasNextLine()) {
         val line = scanner.nextLine().split("\\P{Alnum}+");
         for (val token : line) {
@@ -29,14 +29,15 @@ public class Main {
       return;
     }
 
-    try (FileWriter csvWriter = new FileWriter("out.csv")) {
+    try (BufferedWriter csvWriter = new BufferdFileWriter("out.csv")) {
       int totalWordsQuantity = tokensFreq.values().stream().mapToInt(Integer::intValue).sum();
       tokensFreq.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEachOrdered((entry) -> {
         try {
           csvWriter.write(String.format("%s,%d,%s%n", entry.getKey(), entry.getValue(), String.
                   valueOf((float) entry.getValue() / totalWordsQuantity * 100).replace(',', '.')));
         } catch (IOException e) {
-          System.out.println(e.getMessage());
+          e.printStackTrace();
+          System.out.println();
         }
       });
     } catch (IOException e) {
