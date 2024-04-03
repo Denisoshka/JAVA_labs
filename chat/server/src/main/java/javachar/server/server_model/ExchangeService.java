12182cefa019b573ff6f29a827c896cd8c69b21a
Denis <denis.04@yandex.ru>
2024-04-04 03:23:52 +0700

    do server

--- chat/server/src/main/java/javachar/server/server_model/ExchangeService.java
+++ chat/server/src/main/java/javachar/server/server_model/ExchangeService.java
@@ -1,9 +1,20 @@
 package javachar.server.server_model;
 
+import java.io.*;
 import java.net.Socket;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.*;
 import java.util.function.Consumer;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class ExchangeService implements Runnable, Consumer<Socket>, AutoCloseable {
   private final ExecutorService handleEventsService;
@@ -40,6 +51,7 @@ public class ExchangeService implements Runnable, Consumer<Socket>, AutoCloseabl
 
   @Override
   public void accept(Socket socket) {
+
   }
 
   @Override
@@ -49,39 +61,95 @@ public class ExchangeService implements Runnable, Consumer<Socket>, AutoCloseabl
     }
   }
 
-  public static class Connection implements AutoCloseable, Runnable {
+  public class Connection implements AutoCloseable, Runnable {
+    private final static String COMMAND_TAG = "command";
+    private final static String COMMAND_NAME_ATTR = "name";
+
+
+    public final static int BUFFERSIZE = 41_943_040;
     private final List<Connection> connections;
     private final Socket socket;
+    private final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    private final DocumentBuilder builder = factory.newDocumentBuilder();
+    private DataOutputStream outStream;
+    private DataInputStream inStream;
+    Integer connectionNum = null;
 
-    public Connection(Socket socket, List<Connection> connections) {
+
+    public Connection(Socket socket, List<Connection> connections) throws ParserConfigurationException {
       this.connections = connections;
       this.socket = socket;
+//      this.buffer = new byte[BUFFERSIZE];
     }
 
     @Override
     public void run() {
+      try (DataOutputStream localOStream = new DataOutputStream(socket.getOutputStream());
+           DataInputStream localIStream = new DataInputStream(socket.getInputStream())) {
+        outStream = localOStream;
+        inStream = localIStream;
 
+        installConnection(socket);
+
+
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      } finally {
+        outStream = null;
+        inStream = null;
+      }
+    }
+
+    private void writeMessage(String message, DataOutputStream outStream) throws IOException {
+      int msgLen = message.getBytes().length;
+      outStream.write(msgLen);
+      outStream.write(message.getBytes(), 0, msgLen);
+    }
+
+    private Integer installConnection(Socket socket) {
+      String commandName = null;
+      boolean connectionFailed = false;
+      try {
+        int len = inStream.readInt();
+        byte[] buffer = inStream.readNBytes(len);
+        Document document = builder.parse(new ByteArrayInputStream(buffer, 0, buffer.length));
+        NodeList commandElements = document.getElementsByTagName(COMMAND_TAG);
+
+        if (commandElements.getLength() > 0) {
+          Element commandElement = (Element) commandElements.item(0);
+          commandName = commandElement.getAttribute(COMMAND_NAME_ATTR);
+        }
+        if (!Objects.nonNull(commandName) || commandName.isEmpty()) {
+          connectionFailed = true;
+        }
+        if (!connectionFailed && Objects.isNull(connectionNum = emptySockets.poll(timeout, unit))) {
+          String msg = unableToConnect("failed connection");
+          outStream.write(msg.getBytes().length);
+          return null;
+        }
+
+        try {
+          semaphore.acquire(maxHandleEventsQuantity);
+          if (connectionNum == null) {
+            String msg = unableToConnect("failed connection");
+            outStream.write(msg.getBytes().length);
+            return null;
+          }
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        } finally {
+          semaphore.release(maxHandledUsersQuantity);
+        }
+      } catch (IOException | SAXException | InterruptedException e) {
+        return null;
+
+        throw new RuntimeException("pizda");
+      }
     }
 
     @Override
     public void close() throws Exception {
       socket.close();
-    }
-  }
-
-  private class InstallConnection implements Runnable {
-    @Override
-    public void run() {
-      try {
-        semaphore.acquire(maxHandleEventsQuantity);
-
-      } catch (InterruptedException e) {
-
-//        todo
-        throw new RuntimeException(e);
-      } finally {
-        semaphore.release(maxHandledUsersQuantity);
-      }
     }
   }
 
@@ -101,5 +169,7 @@ public class ExchangeService implements Runnable, Consumer<Socket>, AutoCloseabl
     }
   }
 
-
+  private String unableToConnect(String reason) {
+    return "<error><message>" + reason + "</message></error>";
+  }
 }
