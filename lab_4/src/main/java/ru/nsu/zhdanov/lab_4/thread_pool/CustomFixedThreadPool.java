078120e2c6e2dd754c324713c5ca56070143e88f
Denis <denis.04@yandex.ru>
2024-04-19 21:33:23 +0700

    threadpool

--- lab_4/src/main/java/ru/nsu/zhdanov/lab_4/thread_pool/CustomFixedThreadPool.java
+++ lab_4/src/main/java/ru/nsu/zhdanov/lab_4/thread_pool/CustomFixedThreadPool.java
@@ -3,60 +3,34 @@ package ru.nsu.zhdanov.lab_4.thread_pool;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class CustomFixedThreadPool implements ExecutorService {
+  public final static int POOL_DELAY = 180;
+  public final static int OFFER_DELAY = 30;
+
   private final BlockingQueue<Runnable> taskPool;
-  private volatile boolean shutdowned = false;
-  private volatile int activeTaskQ = 0;
-  private final HashSet<Worker> workers;
+  private volatile boolean isShutdown = false;
+  private volatile boolean terminated = false;
+  private final Set<Worker> workers;
   private final int nThreads;
-  private final ReentrantLock mainLock = new ReentrantLock();
 
   public CustomFixedThreadPool(int nThreads) {
-    this(nThreads, new LinkedBlockingQueue<Runnable>());
+    this(nThreads, new LinkedBlockingQueue<>());
   }
 
   public CustomFixedThreadPool(int nThreads, BlockingQueue<Runnable> taskPool) {
-    this.taskPool = taskPool;
+    this.workers = ConcurrentHashMap.newKeySet();
     this.nThreads = nThreads;
-    this.workers = new HashSet<>(nThreads);
-  }
-
-  private class Worker extends Thread {
-    @Override
-    public void run() {
-      try {
-        while (true) {
-          Runnable task = taskPool.poll(180, TimeUnit.SECONDS);
-          if (task == null) {
-            break;
-          }
-          task.run();
-        }
-      } catch (InterruptedException ignored) {
-      } finally {
-        workerExpired(this);
-      }
-    }
-  }
-
-  private void workerExpired(Worker worker) {
-    synchronized (workers) {
-      workers.remove(worker);
-    }
+    this.taskPool = taskPool;
   }
 
   @Override
   public void shutdown() {
-
+    isShutdown = true;
   }
-//  def x(list: list[int]) -> float
 
   @NotNull
   @Override
@@ -66,48 +40,40 @@ public class CustomFixedThreadPool implements ExecutorService {
 
   @Override
   public boolean isShutdown() {
-    return shutdowned;
+    return isShutdown;
   }
 
   @Override
   public boolean isTerminated() {
-    return shutdowned;
+    return isShutdown;
   }
 
   @Override
   public boolean awaitTermination(long timeout, @NotNull TimeUnit unit) throws InterruptedException {
+    while
     return false;
   }
 
   @NotNull
   @Override
   public <T> Future<T> submit(@NotNull Callable<T> task) {
-    if (activeTaskQ == nThreads) {
-//      taskPool.put();
-    }
-
-    return new FutureTask<>(task);
+    FutureTask<T> futureTask = new FutureTask<>(task);
+    addTask(futureTask);
+    return futureTask;
   }
 
   @NotNull
   @Override
   public <T> Future<T> submit(@NotNull Runnable task, T result) {
+    addTask(task);
     return new FutureTask<>(task, result);
   }
 
   @NotNull
   @Override
   public Future<?> submit(@NotNull Runnable task) {
-    if (task == null) {
-      throw new NullPointerException();
-    }
-    RunnableFuture<Void> futureTask = new FutureTask<>(task, null);
-    execute(futureTask);
-    return futureTask;
-  }
-
-  private Future<?> handleSubmitArgs(){
-    return null;
+    addTask(task);
+    return new FutureTask<>(task, null);
   }
 
   @NotNull
@@ -130,6 +96,7 @@ public class CustomFixedThreadPool implements ExecutorService {
     return null;
   }
 
+  @NotNull
   @Override
   public <T> T invokeAny(@NotNull Collection<? extends Callable<T>> tasks, long timeout, @NotNull TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
     return null;
@@ -137,35 +104,50 @@ public class CustomFixedThreadPool implements ExecutorService {
 
   @Override
   public void execute(@NotNull Runnable command) {
-    taskPool.add(command);
+    addTask(command);
   }
 
-
-
-  /*private final ArrayList<WorkerThread> workers;
-  private final LinkedBlockingQueue<Runnable> queue;
-
-  public CustomThreadPool(int poolSize) {
-    queue = new LinkedBlockingQueue<Runnable>();
-    workers = new ArrayList<>(poolSize);
-
-    for (int i = 0; i < poolSize; ++i) {
-      workers.add(new WorkerThread());
-      workers.getLast().start();
+  private class Worker extends Thread {
+    @Override
+    public void run() {
+      try {
+        while (!isShutdown) {
+          Runnable task = taskPool.poll(POOL_DELAY, TimeUnit.SECONDS);
+          if (task == null) {
+            break;
+          }
+          task.run();
+        }
+      } catch (InterruptedException ignored) {
+      } finally {
+        submitExpiredWorker(this);
+      }
     }
   }
 
-  public void execute(Runnable task) -> xyi{
-    synchronized (queue) {
-      queue.add(task);
-      queue.notify();
-    }
+  private void submitExpiredWorker(Worker worker) {
+    workers.remove(worker);
   }
 
-  public void shutdown() {
-    for (WorkerThread worker : workers) {
-      worker.interrupt();
-    }
+  private void submitNewWorker() {
+    var worker = new Worker();
+    worker.start();
+    workers.add(worker);
   }
-*/
+
+  private void addTask(Runnable task) {
+    boolean rez = taskPool.offer(task);
+    if (rez) {
+      return;
+    }
+
+    if (workers.size() < nThreads) {
+      synchronized (workers) {
+        if (workers.size() < nThreads) {
+          submitNewWorker();
+        }
+      }
+    }
+    taskPool.add(task);
+  }
 }
